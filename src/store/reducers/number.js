c1b3108f6dcd0f1035e9b7fcb408ce3c7e1d62fa
Erick Costa <erickcosta2509@gmail.com>
2021-10-26 23:42:42 -0300

    useReducer #02

---
+++ src/store/reducers/number.js
@@ -0,0 +1,16 @@
+export function numberReducer(state, action) {
+    switch(action.type) {
+        case 'number_add2':
+            return { ...state, number: state.number + 2 }
+        case 'number_multiply7':
+            return { ...state, number: state.number * 7 }
+        case 'number_divide25':
+            return { ...state, number: state.number / 25 }
+        case 'number_parseInt':
+            return { ...state, number: parseInt(state.number) }
+        case 'number_addN':
+            return { ...state, number: state.number + action.payload }
+        default: 
+            return state
+    }
+}
--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,32 +1,7 @@
 import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
-
-const initialState = {
-    cart: [],
-    products: [],
-    user: null,
-    // foco...
-    number: 0
-}
-
-function reducer(state, action) {
-    switch(action.type) {
-        case 'number_add2':
-            return { ...state, number: state.number + 2 }
-        case 'login':
-            return { ...state, user: { name: action.payload } }
-        case 'number_multiply7':
-            return { ...state, number: state.number * 7 }
-        case 'number_divide25':
-            return { ...state, number: state.number / 25 }
-        case 'number_parseInt':
-            return { ...state, number: parseInt(state.number) }
-        case 'number_addN':
-            return { ...state, number: state.number + action.payload }
-        default: 
-            return state
-    }
-}
+import { initialState, reducer } from '../../store'
+import { number_add2, login } from '../../store/actions'
 
 const UseReducer = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState)
@@ -45,9 +20,9 @@ const UseReducer = (props) => {
                 <span className="text">{state.number}</span>
                 <div>
                     <button className="btn"
-                        onClick={() => dispatch({ type: 'login', payload: 'Beatriz' })}>Login</button>
+                        onClick={() => login(dispatch, 'Beatriz')}>Login</button>
                     <button className="btn"
-                        onClick={() => dispatch({ type: 'number_add2' })}>+2</button>
+                        onClick={() => number_add2(dispatch)}>+2</button>
                     <button className="btn"
                         onClick={() => dispatch({ type: 'number_multiply7' })}>x7</button>
                     <button className="btn"
